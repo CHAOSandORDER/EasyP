21038689e0e86acea9685f6f98e183f63ff11fea
Avishek <50791518+CHAOSandORDER@users.noreply.github.com>
2020-03-07 13:19:45 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-pw=$(pwd)
 cd /bin
 mkdir master
 cd -
 sudo updatedb
 pa=$(locate "easy.sh")
 cp $pa /bin/master
+sleep 4
 export PATH=/bin/master:$PATH
+sleep 1
+echo "export PATH=/bin/master:$PATH" >> ~/.bashrc
+sleep 1
+export PATH=/bin/master:$PATH
+sleep 1
 echo "export PATH=/bin/master:$PATH" >> ~/.bashrc
 cd /bin/master
 mv easy.sh E
+export PATH=/bin/master:$PATH
+echo "export PATH=/bin/master:$PATH" >> ~/.bashrc
 E
